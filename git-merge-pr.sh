ee9064865fdd4806e8fd3447c2917fd1c8a02737
Edward Gabriel Moraru <edmoraru@deloittece.com>
2017-04-27 16:28:29 +0300

    fixed script, todo - detect changes for merges and for force push

--- git-merge-pr.sh
+++ git-merge-pr.sh
@@ -5,64 +5,80 @@ set -e
 
 masterBranch=develop
 branch=NO_BRANCH_NAME
+debug=true
 
 #check there's parameter for branch
-if [ $# -le 0 ];
-then
+if [ $# -le 0 ]; then
 	echo "ERROR !"
 	echo "missing parameter for branch name"
 	echo "USAGE: git-merge-pr.sh <branch-name>"
 	exit 1
 else
 	branch=$1
-	echo "merging changes from branch: $branch\n"
+	echo "\nmerging changes from branch: $branch\n"
 fi
 
 #check if current path is valid git repo
-if ([ -d .git ] && (git rev-parse --is-inside-work-tree));
-then
-	echo "current path is a valid git repository\n"
+if ([ -d .git ] && (git rev-parse --is-inside-work-tree)); then
+	if [ $debug ]; then echo "\n******\ncurrent path is a valid git repository\n"
+	fi
 
 	gitRepoURL=$(git config --get remote.origin.url)
-	echo "remote repository url $gitRepoURL\n"
+	if [ $debug ]; then echo "\n******\nremote repository url $gitRepoURL\n"
+	fi
 
 	#check if branch exists on origin
-	if git ls-remote --heads --exit-code $gitRepoURL $branch;
-	then
-		echo "branch $branch exists on remote repo. checking out.\n"
-
+	if git ls-remote --heads --exit-code $gitRepoURL $branch; then
+		if [ $debug ]; then echo "\n******\nbranch $branch exists on remote repo. checking out.\n"
+		fi
 		git checkout $branch
 
 		#rebase latest changes from $masterBranch in the branch
 		git rebase $masterBranch
 
-		#should merge if there are conflicts when merging
-		if $?; then
-			echo "CONFLICTS! starting mergetool\n"
-			git mergetool
+		#should merge if there are conflicts after rebase-ing
+		conflict=false
+		git status --porcelain | while read st file; do
+			if [ "$st" == 'UU' ]; then
+				if [ $debug ]; then echo "\n******\nconflicts detected in file $file"
+				fi
+				conflict=true
+			fi
+		done
+
+		if [ $conflict ]; then
+			if [ $debug ]; then echo "\n******\nstarting mergetool"
+			fi
+		    git mergetool
 		fi
 
 		#check there are changes after rebase
 		localStatus=$(git rev-parse --verify $branch)
 		remoteStatus=$(git rev-parse --verify origin/$branch)
 
-		if [ $localStatus = $remoteStatus ]; then
-		    echo "NO changes to push to origin after rebase\n"
-		else
-		    echo "changes after rebase, force pushing to origin\n"
+		if [ $localStatus != $remoteStatus ]; then
+		    if [ $debug ]; then echo "\n******\nchanges after rebase, force pushing to origin\n"
+			fi
 			git push -f
+		else
+		    echo "NO changes to push to origin after rebase\n"
 		fi
 
 		#move to $masterBranch again
-		echo "checking out $masterBranch\n"
+		if [ $debug ]; then echo "\n******\nchecking out $masterBranch\n"
+		fi
 		git checkout $masterBranch
 
-		# to do : automatically accept the commit message
+		#automatically accept the commit message
 		git merge --no-ff --commit --no-edit $branch
 
-		#?option to do push so I can put multiple PRs into only one
-		#git push
+		#todo: do push optional so I can merge multiple PRs
+		if [ $# == 2 ] && [[ $2 == push ]]; then
+			git push
+		fi
 	else
 		echo "branch $branch was not found on remote repository at $gitRepoURL\n"
 	fi
+else
+	echo "current path is NOT A VALID git repository\n"
 fi
