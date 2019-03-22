861bd1f5ec0002d7a4a672fbacfb0868a04c4e73
Edward Gabriel Moraru <Edward_Gabriel.Moraru@lidl.ro>
2019-03-22 09:47:47 +0200

    removed ldap script

--- ldap-mindshift.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# ldapsearch -x -LLL -h [host] -D [user] -w [password] -b [base DN] -s sub "([filter])" [attribute list]
-ldapsearch -vx -LLL -h ldaps://int.secrz.com:636 -D cms.test-admin -w 8WkCpfFLmZtr
